8a3085260fbb8ffccafac33a8f610b2dd30627f0
sixeyes198 <yossiguadie@icloud.com>
2024-12-22 12:31:46 +0200

    Update

---
+++ frontend/ethio-shop/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Login /> 
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/ethio-shop/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-amber-300 flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Ethio</h2>
+    </div>
+  );
+};
+
+export default Navbar;

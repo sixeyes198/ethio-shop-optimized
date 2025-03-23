795bf3cf89668fd05f5c60ba2c62a66495342dcf
sixeyes198 <yossiguadie@icloud.com>
2025-03-23 12:58:12 +0200

    started the cart

--- frontend/ethio-shop/src/App.jsx
+++ frontend/ethio-shop/src/App.jsx
@@ -2,12 +2,14 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import {BrowserRouter as Router , Routes, Route} from "react-router-dom"
 import SignUp from "./pages/SignUp/SignUp";
+import Cart from "./pages/Cart/Cart";
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/Cart" element={<Cart/>}/>
         <Route path="/dashboard" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
--- frontend/ethio-shop/src/pages/Cart/Cart.jsx
+++ frontend/ethio-shop/src/pages/Cart/Cart.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import img5 from "../../assets/images/img5.jpg"
+import Navbar from "../../components/Navbar/Navbar";
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <>
+    <Navbar/>
+    <section id="home">
+      <div className="relative w-full h-screen ">
+        <img
+          src={img5}
+          alt="women working in field"
+          className="object-cover h-full w-fit"
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center  bg-black/50">
+          <h1 className="text-4xl md:text-5xl font-semibold  text-white text-center px-4">
+            EthioFlavors
+          </h1>
+        </div>      
+      </div>     
+    </section>
+    </>
+  );
+};
 
-export default Cart
+export default Cart;
+

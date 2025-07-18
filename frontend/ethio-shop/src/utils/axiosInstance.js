9f5002a2bff652e2a6ddd945334d882819d75082
sixeyes198 <yossiguadie@icloud.com>
2025-07-18 12:50:38 +0300

    Updated the axiosInstance baseUrl

--- frontend/ethio-shop/src/utils/axiosInstance.js
+++ frontend/ethio-shop/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ethio-shop-b0zq.onrender.com/" || "http://localhost:5000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",

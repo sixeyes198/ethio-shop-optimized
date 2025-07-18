fc7f85c69e9c25f7b23231e1a4062592eb136fdb
sixeyes198 <yossiguadie@icloud.com>
2025-07-18 13:42:17 +0300

    Update

--- backend/Routes/products.js
+++ backend/Routes/products.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 // Get all products
 
-router.get("/", async (req, res) => {
+router.get("/products", async (req, res) => {
   const products = await Product.find();
   res.json(products);
 });
 
 // Add new product
 
-router.post("/", async (req, res) => {
+router.post("/product", async (req, res) => {
   try {
     const newProduct = new Product(req.body);
     const saved = await newProduct.save();
@@ -24,7 +24,7 @@ router.post("/", async (req, res) => {
 
 //Update product
 
-router.put("/:id", async (req, res) => {
+router.put("/product/:id", async (req, res) => {
   try {
     const updated = await Product.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -37,7 +37,7 @@ router.put("/:id", async (req, res) => {
 
 //Delete product
 
-router.delete("/:id", async (req, res) => {
+router.delete("/product/:id", async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
     res.json({ message: "Product deleted" });

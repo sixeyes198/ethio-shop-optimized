afb40e579355c1229d1e4ada23f158d39a646225
sixeyes198 <yossiguadie@icloud.com>
2026-02-15 13:12:06 +0200

    update

--- frontend/ethio-shop/src/utils/axiosInstance.js
+++ frontend/ethio-shop/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ const isLocalhost = window.location.hostname === "localhost"
 const axiosInstance = axios.create({
   baseURL: isLocalhost
     ? "http://localhost:5000"
-    : "https://ethio-shop-backend.onrender.com",
+    : "https://ethio-shop-b0zq.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
